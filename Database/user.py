b8f5fcbbfd782f0b5c82e5f549d65886224b67e8
xCrane-lab <63831341+xCrane-lab@users.noreply.github.com>
2023-04-21 15:24:22 +0300

    Update user.py

--- Database/user.py
+++ Database/user.py
@@ -1,20 +1,9 @@
 import sqlite3
 
-Ncon = sqlite3.connect("Nusers.db")
-Ncur = Ncon.cursor()
-
-Ncur.execute("""CREATE TABLE IF NOT EXISTS users
-                (id INTEGER,  
-                name TEXT, 
-                group_number INTEGER)
-            """)
-
 con = sqlite3.connect("users.db")
 cur = con.cursor()
 
 cur.execute("""CREATE TABLE IF NOT EXISTS users
-                (id INTEGER,  
-                name TEXT, 
-                group_number INTEGER)
+                (id INTEGER)
             """)
 
